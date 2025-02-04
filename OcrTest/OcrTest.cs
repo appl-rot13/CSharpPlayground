ca3e95556c519c42e6ec86f87c4c15c72105e64d
rot <appl.rot13@gmail.com>
2025-02-05 05:45:18 +0900

    Add OcrTest (Move from CSharpBenchmarks)

---
+++ OcrTest/OcrTest.cs
@@ -0,0 +1,54 @@
+﻿
+using Windows.Graphics.Imaging;
+using Windows.Media.Ocr;
+using Windows.Storage;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Tesseract;
+
+[TestClass]
+public class OcrTest
+{
+    private static readonly string ImageFilePath = ".\\target.png";
+
+    [TestMethod]
+    public void OcrWithMicrosoftOcrTest()
+    {
+        Test(filename => this.OcrWithMicrosoftOcr(filename).Result);
+    }
+
+    [TestMethod]
+    public void OcrWithTesseractTest()
+    {
+        Test(this.OcrWithTesseract);
+    }
+
+    private static void Test(Func<string, string> ocr)
+    {
+        ocr(ImageFilePath).ReplaceLineEndings(string.Empty).Trim().Is("OCR Test");
+    }
+
+    private async Task<string> OcrWithMicrosoftOcr(string filename)
+    {
+        var filePath = Path.GetFullPath(filename);
+        var file = await StorageFile.GetFileFromPathAsync(filePath);
+        using var stream = await file.OpenAsync(FileAccessMode.Read);
+
+        var decoder = await BitmapDecoder.CreateAsync(stream);
+        using var bitmap = await decoder.GetSoftwareBitmapAsync();
+
+        var engine = OcrEngine.TryCreateFromUserProfileLanguages();
+        var result = await engine.RecognizeAsync(bitmap);
+        return result.Text;
+    }
+
+    private string OcrWithTesseract(string filename)
+    {
+        using var tesseract = new TesseractEngine(".\\tessdata_fast", "eng");
+
+        using var pix = Pix.LoadFromFile(filename);
+        using var page = tesseract.Process(pix);
+        return page.GetText();
+    }
+}
---
+++ OcrTest/README.md
@@ -0,0 +1,6 @@
+## Result
+
+Windows標準のMicrosoft OCRとTesseract OCRを比較
+- 軽く試した感じではTesseract OCRの方が精度は高そうだが、どちらも実用レベルではない
+- Microsoft OCRでは日本語以外の言語を利用したい場合、Windowsに言語パックをインストールする必要あり
+- Tesseract OCRでは利用したい言語毎に学習済みデータをダウンロードし、所定のフォルダに格納する必要あり
