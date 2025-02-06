b143eec3d9796eae694d13d6f1a8e69ed0f9dae9
rot <appl.rot13@gmail.com>
2025-02-06 11:58:41 +0900

    Add StableDiffutionTest

---
+++ StableDiffusionTest/Configuration/Models/AppSettings.cs
@@ -0,0 +1,84 @@
+﻿
+namespace StableDiffusionTest.Configuration.Models;
+
+using StableDiffusion.NET;
+
+public class AppSettings
+{
+    private string? prompt;
+    private string? negativePrompt;
+
+    public required string ModelPath { get; init; }
+    public string? LoraModelPath { get; init; }
+
+    public string? InitImagePath { get; init; }
+    public required string GeneratedImagePath { get; init; }
+    public required int GeneratedCount { get; init; }
+
+    public required IReadOnlyCollection<string> Prompts { get; init; }
+    public required IReadOnlyCollection<string> NegativePrompts { get; init; }
+
+    public string Prompt => this.prompt ??= string.Join(',', this.Prompts);
+    public string NegativePrompt => this.negativePrompt ??= string.Join(',', this.NegativePrompts);
+
+    public int? Width { get; init; }
+    public int? Height { get; init; }
+    public Sampler? SampleMethod { get; init; }
+    public int? SampleSteps { get; init; }
+    public float? Strength { get; init; }
+    public int? ClipSkip { get; init; }
+    public float? CfgScale { get; init; }
+
+    public DiffusionModel CreateDiffusionModel()
+    {
+        var modelBuilder = ModelBuilder.StableDiffusion(this.ModelPath).WithMultithreading();
+        if (!string.IsNullOrWhiteSpace(this.LoraModelPath))
+        {
+            modelBuilder.WithLoraSupport(this.LoraModelPath);
+        }
+
+        return modelBuilder.Build();
+    }
+
+    public DiffusionParameter CreateDiffusionParameter()
+    {
+        var parameter = DiffusionParameter.SD1Default;
+        if (!string.IsNullOrWhiteSpace(this.NegativePrompt))
+        {
+            parameter.WithNegativePrompt(this.NegativePrompt);
+        }
+
+        if (this.Width.HasValue || this.Height.HasValue)
+        {
+            parameter.WithSize(this.Width, this.Height);
+        }
+
+        if (this.SampleMethod.HasValue)
+        {
+            parameter.WithSampler(this.SampleMethod.Value);
+        }
+
+        if (this.SampleSteps.HasValue)
+        {
+            parameter.WithSteps(this.SampleSteps.Value);
+        }
+
+        if (this.Strength.HasValue)
+        {
+            //parameter.WithStrength(this.Strength.Value);
+            parameter.Strength = this.Strength.Value;
+        }
+
+        if (this.ClipSkip.HasValue)
+        {
+            parameter.WithClipSkip(this.ClipSkip.Value);
+        }
+
+        if (this.CfgScale.HasValue)
+        {
+            parameter.WithCfg(this.CfgScale.Value);
+        }
+
+        return parameter;
+    }
+}
---
+++ StableDiffusionTest/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using HPPH;
+using HPPH.System.Drawing;
+
+using StableDiffusion.NET;
+
+using StableDiffusionTest.Configuration;
+using StableDiffusionTest.Configuration.Models;
+
+try
+{
+    var appSettings = Configuration.Load<AppSettings>("appsettings.json");
+
+    var initImage =
+        string.IsNullOrWhiteSpace(appSettings.InitImagePath)
+            ? null
+            : ImageHelper.LoadImage(appSettings.InitImagePath).ConvertTo<ColorRGB>();
+
+    var parameter = appSettings.CreateDiffusionParameter();
+    using var model = appSettings.CreateDiffusionModel();
+
+    var random = new Random(Environment.TickCount);
+    foreach (var i in Enumerable.Range(1, appSettings.GeneratedCount))
+    {
+        parameter.WithSeed(random.NextInt64(0, long.MaxValue));
+
+        var image =
+            initImage == null
+                ? model.TextToImage(appSettings.Prompt, parameter)
+                : model.ImageToImage(appSettings.Prompt, initImage, parameter);
+
+        var filePath = string.Format(appSettings.GeneratedImagePath, i);
+        await File.WriteAllBytesAsync(filePath, image.ToPng());
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+    Console.ReadLine();
+}
